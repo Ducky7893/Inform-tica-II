07a2a8f36eb4aff82f509eb1c773ff995fa30a86
Eduar Gutierrez <eduardbv2007@gmail.com>
2026-03-19 00:40:41 -0500

    Se agrega los archivos juego y juego h, estos permitiran la debida conexion entre los archivos para que el juego se pueda ejecutar correctamente, se eliminan archivos rebundantes que estaban en git hub

---
+++ Desafio I/juego.h
@@ -0,0 +1,33 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
+#include "tablero.h"
+#include "pieza.h"
+
+class Juego {
+public:
+    void inicializar(int ancho, int alto);
+    void liberar();
+    void cicloDelJuego();
+
+private:
+    Tablero     tablero;
+    Pieza       piezas[NUM_PIEZAS];
+    PiezaActiva piezaActiva;
+    int         puntaje;
+    bool        gameOver;
+
+    void procesarAccion(char accion);
+    void moverIzquierda();
+    void moverDerecha();
+    void moverAbajo();
+    void rotar();
+
+    bool puedeColocar(const PiezaActiva* pa) const;
+    void fijarPieza();
+    void eliminarLineas();
+    void spawnNuevaPieza();
+    void imprimirEstado() const;
+};
+
+#endif // JUEGO_H
