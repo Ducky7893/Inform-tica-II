abc55912f275c9f9c67c5293215cb7cdb4e58a0b
estoico-forever <diego.barrionuevo@udea.edu.co>
2026-03-18 23:54:15 -0500

    refactor: implementación de Tablero con aritmética de punteros y bitwise
    actualize  el módulo Tablero para cumplir con las restricciones del Desafío I.
    
    Se migró el manejo de memoria a aritmética de punteros pura *(p + i).
    
    Implementación de funciones de limpieza y verificación de filas mediante operadores a nivel de bits (&, |, ~).

--- Desafio I/tablero.cpp
+++ Desafio I/tablero.cpp
@@ -9,8 +9,11 @@ void inicializarTablero(Tablero* t, int ancho, int alto) {
     int totalBytes = alto * t->bytesPorFila;
     t->memoria = new uint8_t[totalBytes];
 
-    for (int i = 0; i < totalBytes; i++)
-        t->memoria[i] = 0;
+   
+    uint8_t* ptr = t->memoria;
+    for (int i = 0; i < totalBytes; i++) {
+        *(ptr + i) = 0;
+    }
 }
 
 void liberarTablero(Tablero* t) {
@@ -23,7 +26,8 @@ void imprimirTablero(const Tablero* t) {
         std::cout << '|';
 
         for (int b = 0; b < t->bytesPorFila; b++) {
-            uint8_t byteActual = t->memoria[fila * t->bytesPorFila + b];
+            // Corregi: Aritmética de punteros en lugar de índices [] xd
+            uint8_t byteActual = *(t->memoria + (fila * t->bytesPorFila) + b);
 
             for (int bit = 7; bit >= 0; bit--) {
                 uint8_t mascara = (uint8_t)(1 << bit);
@@ -41,25 +45,29 @@ void imprimirTablero(const Tablero* t) {
 
 bool filaCompleta(const Tablero* t, int fila) {
     for (int b = 0; b < t->bytesPorFila; b++) {
-        // 0xFF = todos los bits en 1; si algún byte falla, la fila no está llena
-        if (t->memoria[fila * t->bytesPorFila + b] != 0xFF)
+        // Corregi por : Aritmética de punteros
+        if (*(t->memoria + (fila * t->bytesPorFila) + b) != 0xFF)
             return false;
     }
     return true;
 }
 
 void eliminarFila(Tablero* t, int fila) {
-    // Desplazar filas superiores una posición hacia abajo
+    
     for (int f = fila; f > 0; f--) {
+        uint8_t* filaDestino = t->memoria + (f * t->bytesPorFila);
+        uint8_t* filaOrigen = t->memoria + ((f - 1) * t->bytesPorFila);
+
         for (int b = 0; b < t->bytesPorFila; b++) {
-            t->memoria[f * t->bytesPorFila + b] =
-                t->memoria[(f - 1) * t->bytesPorFila + b];
+            *(filaDestino + b) = *(filaOrigen + b);
         }
     }
     insertarFilaCero(t);
 }
 
 void insertarFilaCero(Tablero* t) {
+  
+    uint8_t* primeraFila = t->memoria;
     for (int b = 0; b < t->bytesPorFila; b++)
-        t->memoria[b] = 0;
+        *(primeraFila + b) = 0;
 }
