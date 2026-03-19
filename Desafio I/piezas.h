372eefb32568998b7db6f51399f78ec13781a000
Eduar Gutierrez <eduardbv2007@gmail.com>
2026-03-19 00:52:51 -0500

    Muchos errores con git hub guardando

--- Desafio I/piezas.h
+++ Desafio I/piezas.h
@@ -1,41 +1,39 @@
 #ifndef PIEZAS_H
 #define PIEZAS_H
-
+ 
 #include <stdint.h>
-
+ 
 const int MAX_FILAS_PIEZA = 4;
 const int NUM_PIEZAS      = 6;
-
-struct Pieza {
+-------------------------------------------------------------------------
+class Pieza {
+public:
     char    id;
     int     anchoBase;
     int     altoBase;
     uint8_t formaBase[MAX_FILAS_PIEZA];
 };
-
-struct Forma {
+ -------------------------------------------------------
+class Forma {
+public:
     int     ancho;
     int     alto;
     uint8_t filas[MAX_FILAS_PIEZA];
 };
-
-struct PiezaActiva {
+ 
+class PiezaActiva {
+public:
     Pieza* tipo;
     int    rotActual;
     int    x;
     int    y;
 };
-
-void inicializarPiezas(Pieza* piezas);
-
-int obtenerBit(const Forma* f, int fila, int col);
-
-Forma rotarForma(const Forma* f);
-
-Forma obtenerFormaActual(const Pieza* pieza, int numRotaciones);
-
-PiezaActiva generarNuevaPieza(Pieza* piezas, int anchoTablero);
-
-void rotarPieza(PiezaActiva* pa);
-
+ 
+void        inicializarPiezas   (Pieza* piezas);
+int         obtenerBit          (const Forma* f, int fila, int col);
+Forma       rotarForma          (const Forma* f);
+Forma       obtenerFormaActual  (const Pieza* pieza, int numRotaciones);
+PiezaActiva generarNuevaPieza   (Pieza* piezas, int anchoTablero);
+void        rotarPieza          (PiezaActiva* pa);
+ 
 #endif
