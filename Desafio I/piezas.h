2fc02e22e2b1178381c5301d2093f0eed7ba6045
estoico-forever <diego.barrionuevo@udea.edu.co>
2026-03-21 01:11:41 -0500

    correcion de errores con ansi c
    cambie algunos problemas de anci c y de estructuras

--- Desafio I/piezas.h
+++ Desafio I/piezas.h
@@ -1,11 +1,12 @@
-#ifndef PIEZAS_H
-#define PIEZAS_H
- 
-#include <stdint.h>
- 
+#ifndef PIEZA_H
+#define PIEZA_H
+
+#include <cstdint> // corregi la cabecera 
+
 const int MAX_FILAS_PIEZA = 4;
 const int NUM_PIEZAS      = 6;
--------------------------------------------------------------------------
+
+// cambie por class
 class Pieza {
 public:
     char    id;
@@ -13,14 +14,14 @@ public:
     int     altoBase;
     uint8_t formaBase[MAX_FILAS_PIEZA];
 };
- -------------------------------------------------------
+
 class Forma {
 public:
     int     ancho;
     int     alto;
     uint8_t filas[MAX_FILAS_PIEZA];
 };
- 
+
 class PiezaActiva {
 public:
     Pieza* tipo;
@@ -28,12 +29,13 @@ public:
     int    x;
     int    y;
 };
- 
-void        inicializarPiezas   (Pieza* piezas);
-int         obtenerBit          (const Forma* f, int fila, int col);
-Forma       rotarForma          (const Forma* f);
-Forma       obtenerFormaActual  (const Pieza* pieza, int numRotaciones);
-PiezaActiva generarNuevaPieza   (Pieza* piezas, int anchoTablero);
-void        rotarPieza          (PiezaActiva* pa);
- 
+
+// Prototipos de funciones
+void inicializarPiezas(Pieza* piezas);
+int obtenerBit(const Forma* f, int fila, int col);
+Forma rotarForma(const Forma* f);
+Forma obtenerFormaActual(const Pieza* pieza, int numRotaciones);
+PiezaActiva generarNuevaPieza(Pieza* piezas, int anchoTablero);
+void rotarPieza(PiezaActiva* pa);
+
 #endif
